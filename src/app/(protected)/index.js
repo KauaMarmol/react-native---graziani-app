3a76e881bad32db29aac47436f6ff1a7ec1fe0b1
KauaMarmol <159555487+KauaMarmol@users.noreply.github.com>
2024-09-04 11:44:23 +0000

    {feat} PagerView in the Home

--- src/app/(protected)/index.js
+++ src/app/(protected)/index.js
@@ -1,13 +1,10 @@
-import { Button, Text, View } from "react-native";
-import { useAuth } from "../../hooks/Auth/index";
+import { View } from "react-native";
+import { Banner } from "../../components/Banner";
 
 export default function Home() {
-  const {signOut} = useAuth();
-  
   return (
-    <View style={{flex: 1, justifyContent:'center', alignItemns: 'center'}}>
-      <Text>Home</Text>
-      <Button title="Sair" onPress={() => signOut()} />
+    <View style={{flex: 1}}>
+      <Banner />
     </View>
   );
 }
