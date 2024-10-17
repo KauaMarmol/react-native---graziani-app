2b579ad748ff7f78053028998c183e82f6c06f88
KauaMarmol <159555487+KauaMarmol@users.noreply.github.com>
2024-10-17 20:18:32 +0000

    [fix] Shield Número recibo of screen Payments

--- src/app/(protected)/payment.js
+++ src/app/(protected)/payment.js
@@ -1,5 +1,5 @@
 import { router } from "expo-router";
-import { StyleSheet, Button, TextInput, View, Text, Platform, KeyboardAvoidingView } from "react-native";
+import { StyleSheet, Button, TextInput, View, Text, Platform, KeyboardAvoidingView, Alert } from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useEffect, useRef, useState } from "react";
 import { Picker } from '@react-native-picker/picker';
@@ -15,7 +15,8 @@ const paymentSchema = z.object({
   user_id: z.number().int().positive(),
   user_cadastro: z.number().int().positive(),
   data_pagamento: z.date(),
-  observacao: z.string(),
+  numero_recibo: z.string(),
+  observacao: z.string().optional(),
 });
 
 export default function Payment() {
@@ -25,6 +26,7 @@ export default function Payment() {
   const [data, setData] = useState(new Date());
   const [viewCalendar, setViewCalendar] = useState(false);
   const [observacao, setObservacao] = useState("");
+  const [numeroRecibo, setNumeroRecibo] = useState("");
   const valueRef = useRef();
   const { user } = useAuth()
   const { createPayment } = usePaymentsDatabase();
@@ -88,6 +90,7 @@ export default function Payment() {
       user_cadastro: Number(user.user.id),
       valor_pago: convertValue(valor),
       data_pagamento: data,
+      numero_recibo: numeroRecibo,
       observacao,
     };
     try {
@@ -98,8 +101,10 @@ export default function Payment() {
       setId(sugestoes[0].id); 
       setData(new Date());
       setObservacao("");
+      setNumeroRecibo("");
       valueRef?.current?.focus();
     } catch (error) {
+      Alert.alert("Erro", `Erro ao inserir pagamento: ${error.message}`);
       console.log(error);
     }
   };
@@ -111,6 +116,10 @@ export default function Payment() {
         <View style={styles.inputView}>
           <Ionicons name="wallet-outline" size={24} color="black" />
           <TextInput placeholder="Valor" keyboardType="decimal-pad" style={styles.inputValor} value={valor} onChangeText={(newValue) => handleChangeValor(newValue)} ref={valueRef} />
+        </View>
+        <View style={styles.inputView}>
+          <Ionicons name="cash-outline" size={24} color="black" />
+          <TextInput placeholder="Número do Recibo" keyboardType="decimal-pad" style={styles.inputValor} value={numeroRecibo} onChangeText={setNumeroRecibo} />
         </View>
         <View style={styles.inputView}>
           <Picker selectedValue={id} onValueChange={(itemValue, index) => {
